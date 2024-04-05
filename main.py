6a6f6207790bec0774bd0535c5f8836ac1af43f1
viet <vietvodoi1232@gmail.com>
2024-04-05 21:12:35 +0700

    add huong dan

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ def main(web_config, truyen, start_chapter=1, end_chapter=21, number_chapter_in_
 
     options = option_chrome(path_to_save_mp3)
     # thết lập chrome
-    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
     trans = web_config['trans'] if 'trans' in web_config else False
     print('init successful')
     # # lay text luu vao thu muc text
-    # save_text(driver, truyen, web_config, start_chapter, end_chapter)
-    #
+    save_text(driver, truyen, web_config, start_chapter, end_chapter)
+
     # # dich
     if trans:
         driver = webdriver.Chrome(options=options)
@@ -52,5 +52,7 @@ def main(web_config, truyen, start_chapter=1, end_chapter=21, number_chapter_in_
     make_video(1,truyen['image_path'],path_to_save_mp3,output_video_folder,number_chapter_in_video)
 
     dele_mp3(path_to_save_mp3)
+
+
 
 main(web['qianyege'], stories['chien_tranh_co_khi'], 1,21,10)
